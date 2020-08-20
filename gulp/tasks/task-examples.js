f2b111a5d04282c09a881d8ff7c7339f58b8236f
Jan Andrle <andrle.jan@centrum.cz>
2020-08-20 13:35:29 +0200

    more examples + move examples src folder

--- gulp/tasks/task-examples.js
+++ gulp/tasks/task-examples.js
@@ -3,33 +3,15 @@ module.exports= function({app, $gulp_folder, gulp, error, $g, $o, $run}){
     /* jshint -W061 */const gulp_place= $g.place({ variable_eval: (str)=> eval(str) });/* jshint +W061 */
     const 
         to_folder= "docs/",
-        from_folder= "docs/examples/";
-    let links_paragraphs_buffer= [];
+        from_folder= "src_docs/examples/";
     return function(cb){
         gulp.src([from_folder+"*.html", '!'+from_folder+'*.sub.html'])
             .pipe(gulp_place({ folder: from_folder, string_wrapper: '' }))
-            .pipe($g.replace(/(\/\/EXAMPLE START|<!-- example source links -->)/g, addSourceLink))
             .pipe(gulp.dest(to_folder))
             .on('end', function(){
-                gulp.src([app.directories.bin+app.name+"-module.min.js"])
-                .pipe(gulp.dest(to_folder))
+                gulp.src([app.directories.bin+app.name+"-namespace.min.js"])
+                .pipe(gulp.dest(to_folder+"examples_files/"))
                 .on('end', cb);
             });
     };
-    function addSourceLink(match, type, chars, file_text){
-        if(type==="<!-- example source links -->"){
-            return links_paragraphs_buffer.shift();
-        }
-        const line= String((t=> t&&t.length+1||1)(file_text.substring(0, chars).match(/\r?\n/g)));
-        const link= "https://github.com/jaandrle/dollar_dom_component/blob/master/docs/examples.html#L"+line;
-        links_paragraphs_buffer.push(`<p class="notes"><a href="${link}" target="_blank">View with highliting (link to source of this file)</a></p>`);
-        return match;
-    }
-    function toELID(str){ return "example_element_"+pad(str, 3, "0"); }
-    function toKeyUp(el_str){ return `onKeyup(this, '${el_str}')`; }
-    function pad(n, width, z) {
-        z = z || '0';
-        n = n + '';
-        return n.length >= width ? n : new Array(width - n.length + 1).join(z) + n;
-    }
 };
